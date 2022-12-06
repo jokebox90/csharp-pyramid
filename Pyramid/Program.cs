e8898c3a517f8b5e6886185313c2093015a982fa
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-12-06 22:13:17 +0100

    Exemple C#:  les opérateurs

---
+++ Pyramid/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int e = 11;
+int f = 3;
+
+WriteLine("Coucou les opérateurs. Tchu !");
+
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+
+WriteLine("Au revoir les opératuers. Tchu !");
