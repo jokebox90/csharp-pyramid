fc35395a8c96d4afa91aa7575ad4f0c30cdc6e26
Jokebox90 <dev.jvagnier@gmail.com>
2022-12-07 22:36:10 +0100

    Ajoute des fonctions

--- Pyramid/Program.cs
+++ Pyramid/Program.cs
@@ -1,15 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int e = 11;
-int f = 3;
+WriteLine("Hell'3, w0rld!");
 
-WriteLine("Coucou les opérateurs. Tchu !");
+static string ToBinaryString(int value)
+{
+    return Convert.ToString(value, toBase: 2).PadLeft(8, '0');
+}
 
-WriteLine($"e is {e}, f is {f}");
-WriteLine($"e + f = {e + f}");
-WriteLine($"e - f = {e - f}");
-WriteLine($"e * f = {e * f}");
-WriteLine($"e / f = {e / f}");
-WriteLine($"e % f = {e % f}");
+// Définition d'une fonction
+void MainProgram()
+{
+    int a = 11;
+    int b = 6;
 
-WriteLine("Au revoir les opératuers. Tchu !");
+    WriteLine($"a = {a}");
+    WriteLine($"b = {b}");
+    WriteLine($"bin(a): {ToBinaryString(a)}");
+    WriteLine($"bin(b): {ToBinaryString(b)}");
+    WriteLine($"a & b = {a & b}"); // 2-bit column only e.g. 00000010
+    WriteLine($"a | b = {a | b}"); // 8, 4, and 2-bit columns e.g. 00001110
+    WriteLine($"a ^ b = {a ^ b}"); // 8 and 4-bit columns e.g. 00001100
+}
+
+WriteLine("The main program...");
+MainProgram();
+WriteLine("Done!");
