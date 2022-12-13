7b5c655add2c0cca7aff5e49335766c4b69298b4
Jokebox90 <dev.jvagnier@gmail.com>
2022-12-13 14:28:10 +0100

    Début de dessin de pyramide

--- Pyramid/Program.cs
+++ Pyramid/Program.cs
@@ -28,11 +28,11 @@ static void DoStep(int x, int y, int stepX = 0, int stepY = 0)
 {
     if (stepX < x)
     {
-        DoLine(x, y, stepX, stepY);
+        DoColumn(x, y, stepX, stepY);
     }
     else if (stepX == x && stepY < y)
     {
-        DoColumn(x, y, stepX, stepY);
+        DoLine(x, y, stepX, stepY);
     }
     else
     {
@@ -40,9 +40,13 @@ static void DoStep(int x, int y, int stepX = 0, int stepY = 0)
     }
 }
 
-static void DoLine(int x, int y, int stepX = 0, int stepY = 0)
+static void DoColumn(int x, int y, int stepX = 0, int stepY = 0)
 {
-    if (stepY == 0 || stepY == (y) || stepX == 0 || stepX == (x - 1))
+    if (stepY > 2 && stepY < y && (stepX == (x / 2) + (stepY - 1) * 2) || (stepX == (x / 2) - (stepY - 1) * 2))
+    {
+        Console.Write("+");
+    }
+    else if (stepY == 0 || stepY == (y) || stepX == 0 || stepX == (x - 1))
     {
         Console.Write("o");
     }
@@ -54,7 +58,7 @@ static void DoLine(int x, int y, int stepX = 0, int stepY = 0)
     DoStep(x, y, stepX + 1, stepY);
 }
 
-static void DoColumn(int x, int y, int stepX = 0, int stepY = 0)
+static void DoLine(int x, int y, int stepX = 0, int stepY = 0)
 {
     Console.Write("\r\n");
     DoStep(x, y, 0, stepY + 1);
